f0f77a8d8cf02d3f6f287e121b1ea191f20f0a4b
dpardon <dpardon@visioneo.org>
2014-04-01 20:23:40 +0200

    Update vui.js

--- vui.js
+++ vui.js
@@ -1,14 +1,13 @@
 (function($) {
-    
+  //template variables 
   var genLI='<li id="@prefix@@value@" class="ui-widget-content ui-corner-all"><span class="ui-icon ui-icon-arrowthick-2-n-s"></span><span title="@label@">@value@</span><span class="ui-icon ui-icon-close" title="Remove item"></span></li>',
-      container='<div class="vui vui-multiselect"><ul class="ui-widget"></ul><button>@button@</button></div>',
-      button='<button>Run stuff</button>';
+      container='<div class="vui vui-multiselect"><ul class="ui-widget"></ul><button>@button@</button></div>';
 
     
     $.fn.visioMultiselect=function(options){
         
         var settings = $.extend({
-            // These are the defaults.
+            // These are the defaults options
             duration: 2000,
             eventID:"vbroadcast",
             prefix:"vui_multi_li_",
@@ -21,6 +20,7 @@
         }, options ),
             self=this,
             getValues=function(){
+                //return values currently selected in an array, in the same order as jquery UI widget
                 var values= self.find("ul").first().sortable("toArray"),
                     prefix=new RegExp("^"+settings.prefix); 
                  for(var i = 0, len = values.length; i < len; i++) {
@@ -29,11 +29,11 @@
                 return values;
             },
             addLine=function(e){
-    
+              //add a new line to the widget if the ID is not already present
               var newID="#"+settings.prefix+e.message.value.replace(/\./g,'\\.');
           
               if( !self.find(newID).length){
-                  //this is a new row
+                  //ID does not exist, this is a new row
                   var newli=genLI.replace(/@prefix@/g,settings.prefix).replace(/@value@/g,e.message.value).replace(/@label@/g,e.message.label);
                   $(newli).appendTo(self.find("ul").first()).effect(settings.effect, {}, settings.duration).hover(function() {
                           $( this ).toggleClass( "ui-state-hover" );
@@ -41,6 +41,7 @@
                       
                         $(newID).effect('fade', {}, 250, function(){
                                 $(this).remove();
+                                //button should be disabled if there is no entry left in the list
                                 if( !self.find('li').length){
                                     self.find('button').button("disable");
                                 }                               
@@ -54,8 +55,10 @@
               }
           }; 
         
+        //initialize both container and buttons
         self.html(container).find('button').html(settings.button).click(function(){settings.action(getValues())}).button({disabled:true,icons:{ primary: settings.buttonIcon }});
         
+        //event listener
         $(document).on(settings.eventID,function(event){
             //console.log(event);
             addLine(event);
